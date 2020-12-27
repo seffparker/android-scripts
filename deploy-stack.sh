814bffcdb10ef1743f5c21ba66209ba68f74af10
Seff <painkalshafeer@gmail.com>
2020-12-27 10:44:38 +0530

    Update deploy-stack.sh

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -18,9 +18,9 @@ mkdir /data/bin /data/xbin /data/root
 ln -s /data/adb/magisk/busybox /data/bin/busybox
 /data/bin/busybox --install -s /data/bin/
 
-echo "Copying assets into default workdir..."
-mkdir -p /sdcard/lab/android
-cp -rf ./* sdcard/lab/android/
+echo "Copying assets..."
+cp -rf data/* /data/
+cp -rf sdcard/* /sdcard/
 
 echo "Installing user scripts and envs..."
 rm -f /data/data/com.termux/files/home/.bashrc
