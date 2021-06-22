ea73e3c348ba61166745a1482d63f1ff5c66871a
Seff Parker <painkalshafeer@gmail.com>
2021-06-22 12:20:13 +0530

    updated for set-log

--- data/adb/modules/batmobile-mods/service.sh
+++ data/adb/modules/batmobile-mods/service.sh
@@ -1,11 +1,15 @@
 #!/system/bin/sh
 
 {
+    # Kill audioserver PID if it was initialized already
+    SERVERPID=$(pidof audioserver)
+    [ "$SERVERPID" ] && kill $SERVERPID
+
     echo "$(/data/xbin/logdate) Waiting for boot completion"
     while [ "$(getprop sys.boot_completed)" != "1" ]
         do sleep 1
         done
     sleep 5
     export PATH=$PATH:/data/xbin
-    /data/bin/bash /data/xbin/post-boot autorun | /data/xbin/logdate 
-} > /data/media/0/lab/android/log/post-boot.log
+    /data/bin/bash /data/xbin/post-boot autorun | /data/xbin/set-log
+} &> /data/media/0/lab/android/log/post-boot.log
